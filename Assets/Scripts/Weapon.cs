b22bbdb0866f8493ea34351eb120616f7159bcd5
Pradyum Mistry <39883432+AltF4-Games@users.noreply.github.com>
2023-12-28 17:40:53 +0530

    Weapon Shooting & Fx

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,35 +1,103 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
     [SerializeField] private Transform firePoint;
     [SerializeField] private Transform cameraT;
-    [SerializeField] private int bullets = 7;
+    [SerializeField] private Animator weaponAnimator;
+    [SerializeField] private LineRenderer lineRenderer;
+    [SerializeField] private GameObject muzzleFlash;
+    [SerializeField] private int maxBullets = 7;
     [SerializeField] private float reloadTime = 1.0f;
+    [SerializeField] private float fireRate = 0.3f;
     [SerializeField] private float damage = 0.5f;
     [SerializeField] private float maxRange = 300f;
+    private int bullets;
     private bool isReloading = false;
+    private bool canShoot = true;
 
     void Start()
     {
-        
+        bullets = maxBullets;
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.positionCount = 2;
     }
 
     void Update()
     {
-        if (isReloading) return;
+        if (isReloading || !canShoot || bullets <= 0) return;
+
         if (Input.GetMouseButtonDown(0))
         {
-            Ray ray = new Ray(cameraT.position, cameraT.forward);
-            RaycastHit hit;
-            bool rayCast = Physics.Raycast(ray,out hit, maxRange);
-            Debug.DrawRay(cameraT.position, cameraT.forward * maxRange,Color.red,4f);
-            if (rayCast)
-            {
-                Debug.Log(hit.transform.name);
-            }
+            StartCoroutine(Shoot());
         }
+
+        if (Input.GetKeyDown(KeyCode.R) && bullets < maxBullets)
+        {
+            StartCoroutine(Reload());
+        }
+    }
+
+    private IEnumerator Shoot()
+    {
+        canShoot = false;
+        weaponAnimator.SetTrigger("Fire");
+
+
+        Ray ray = new Ray(cameraT.position, cameraT.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, maxRange))
+        {
+            // TODO: Handle damage to the hit object (hit.transform)
+            // Debug.Log("Hit: " + hit.transform.name);
+        }
+
+        bullets--;
+
+        if (bullets <= 0)
+        {
+            StartCoroutine(Reload());
+        }
+
+        // Visualize the ray with a Line Renderer
+
+        muzzleFlash.SetActive(true);
+        Vector3 hitLoc = (hit.point == Vector3.zero) ? cameraT.forward * maxRange : hit.point;
+        lineRenderer.enabled = true;
+        lineRenderer.SetPosition(0, firePoint.position);
+        lineRenderer.SetPosition(1, hitLoc);
+
+        //lineRenderer.startColor = Color.red;
+        //lineRenderer.endColor = Color.red;
+        lineRenderer.startWidth = 0.1f;
+        lineRenderer.endWidth = 0.1f;
+
+        StartCoroutine(DisableLineRenderer());
+
+        yield return new WaitForSeconds(fireRate); // Recovery Time
+
+        canShoot = true;
+    }
+
+    IEnumerator DisableLineRenderer()
+    {
+        yield return new WaitForSeconds(fireRate);
+        lineRenderer.enabled = false;
+        muzzleFlash.SetActive(false);
+    }
+
+    private IEnumerator Reload()
+    {
+        if (isReloading)
+            yield break; // Exit if already reloading
+
+        isReloading = true;
+        weaponAnimator.SetTrigger("Reload");
+        yield return new WaitForSeconds(reloadTime);
+        bullets = maxBullets;
+        isReloading = false;
     }
 }
